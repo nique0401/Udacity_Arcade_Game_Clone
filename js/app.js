9d1fbdd000bf975fd60b69c0531dc2bc26c66707
Dominique Schaefer <Nique0401@yahoo.com>
2018-05-24 07:45:04 -0700

    created the starter page and hidden/visible fields needed to delay game start

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
-let lives;
+let lives = 3;
 let score = 0;
 let level = 'easy';
-let character= 'char-boy.png';
+let character = 'char-boy.png';
 let allEnemies = [];
-
+let play = false;
+let selectedChar; //Used as pointer for the selected sprite URL in array
+let chars = [ //Array of URLs for player and NPC sprites
+    'images/char-boy.png',
+    'images/char-cat-girl.png',
+    'images/char-horn-girl.png',
+    'images/char-pink-girl.png',
+    'images/char-princess-girl.png'
+];
+let name = 'guest';
 
 
 
 
 // Enemies our player must avoid
-var Enemy = function(locX, locY) {
+var Enemy = function(locX, locY, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -17,6 +26,7 @@ var Enemy = function(locX, locY) {
     this.sprite = 'images/enemy-bug.png';
     this.x = locX;
     this.y = locY;
+    this.speed = speed
     this.argument1 = locX;
     this.argument2 = locY;
 };
@@ -25,42 +35,48 @@ var Enemy = function(locX, locY) {
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
 
-  //set level
-  if (level === 'easy') {
-    for(i=0;i<allEnemies.length;i++) {
-      allEnemies[i].speed=140;
+    //set level
+    if (level === 'easy') {
+        for (i = 0; i < allEnemies.length; i++) {
+            allEnemies[i].speed = 140;
+        }
     }
-  }
-  if (level === 'medium') {
-    for(j=0;j<allEnemies.length;j++) {
-      allEnemies[j].speed=190;
+    if (level === 'medium') {
+        for (j = 0; j < allEnemies.length; j++) {
+            allEnemies[j].speed = 190;
+        }
     }
-  }
-  if (level === 'hard') {
-    for(k=0;k<allEnemies.length;k++) {
-      allEnemies[k].speed=250;
+    if (level === 'hard') {
+        for (k = 0; k < allEnemies.length; k++) {
+            allEnemies[k].speed = 250;
+        }
     }
-  }
 
-// multiply any movement by the dt parameter to get same speed for all pc's
-  this.x = this.x+(this.speed*dt);
+    // multiply any movement by the dt parameter to get same speed for all pc's
+    this.x = this.x + (this.speed * dt);
 
-  //resets position if enemy reaches end of screen
-  if (this.x >=600) {
-    this.reset();
-  }
+    //resets position if enemy reaches end of screen
+    if (this.x >= 600) {
+        this.reset();
+    }
 
 
-  //collisions with Enemies, reset players position
-  if( player.x >= this.x -35 && player.x <=this.x + 35 ){
-          if( player.y >= this.y -35 && player.y <=  this.y+35 ){
-              player.x = 200;
-              player.y = 400;
-          }
-      }
+    //collisions with Enemies, reset players position
+    if (player.x >= this.x - 35 && player.x <= this.x + 35) {
+        if (player.y >= this.y - 35 && player.y <= this.y + 35) {
+            if (lives > 1) {
+                player.x = 200;
+                player.y = 400;
+                lives = lives - 1;
+                $('#livesLeft').text(lives);
+            } else {
+                endGame();
+            }
+        }
+    }
 };
-Enemy.prototype.reset = function(){
-    this.x = randomLocation(-400,-100);
+Enemy.prototype.reset = function() {
+    this.x = randomLocation(-400, -100);
     this.y = this.argument2;
 };
 
@@ -73,36 +89,34 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function(x, y) {
-  this.sprite = 'images/'+character;
-  this.x = x;
-  this.y = y;
+    this.sprite = 'images/' + character;
+    this.x = x;
+    this.y = y;
 };
-Player.prototype.update = function(){
+Player.prototype.update = function() {
     //changes, handles and displays the score
-winGame()
+    winGame()
 
 };
-Player.prototype.render = function(){
+Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-Player.prototype.handleInput = function(rcv){
-    if( rcv === 'left' && this.x > 0 )
-        this.x = this.x - 20;
-    else if( rcv === 'right' && this.x < 400)
-        this.x = this.x + 20;
-    else if( rcv === 'up' && this.y > -50)
-        this.y = this.y - 20;
-    else if( rcv === 'down' && this.y < 400)
-        this.y = this.y + 20;
+Player.prototype.handleInput = function(rcv) {
+    if (rcv === 'left' && this.x > 0)
+        this.x = this.x - 100;
+    else if (rcv === 'right' && this.x < 400)
+        this.x = this.x + 100;
+    else if (rcv === 'up' && this.y > -50)
+        this.y = this.y - 85;
+    else if (rcv === 'down' && this.y < 400)
+        this.y = this.y + 85;
 };
-Player.prototype.reset = function(){
+Player.prototype.reset = function() {
     this.x = 200;
     this.y = 400;
 };
 
-var showResult = function(){
-     $('h3').css("display","none").text("");
-};
+
 
 // Now instantiate your objects.
 let player = new Player(200, 400);
@@ -111,7 +125,7 @@ let enemy2 = new Enemy(-150, 145);
 let enemy3 = new Enemy(-480, 60);
 
 
-allEnemies.push(enemy1,enemy2,enemy3);
+allEnemies.push(enemy1, enemy2, enemy3);
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
@@ -136,25 +150,112 @@ window.addEventListener('keydown', function(e) {
     }
 });
 
-function randomLocation(min,max) {
-  return Math.random() * (max-min) +min;
+function randomLocation(min, max) {
+    return Math.random() * (max - min) + min;
 }
 
 function winGame() {
-  if (player.y < 0) {
-    player.x=200;
-    player.y=400;
-    if ((score/100) % 3 === 0){
-      allEnemies.push(new Enemy(randomLocation(-500,-100), 60));
+    if (player.y < 0) {
+        player.x = 200;
+        player.y = 400;
+        if ((score / 100) % 3 === 0) {
+            allEnemies.push(new Enemy(randomLocation(-500, -100), 60));
+        }
+        if ((score / 100) % 4 === 0) {
+            allEnemies.push(new Enemy(randomLocation(-500, -100), 145));
+        }
+        if ((score / 100) % 5 === 0) {
+            allEnemies.push(new Enemy(randomLocation(-500, -100), 230));
+        }
+        //
+        score = score + 100;
+        if (play == true) {
+            $('#score').css("display", "block");
+            $('#lives').css("display", "block");
+            $('#lvlDifficulty').css("display", "block");
+            $('.scoreboard').text(score);
+            $('#livesLeft').text(lives);
+
+        }
     }
-    if ((score/100) % 4 === 0){
-      allEnemies.push(new Enemy(randomLocation(-500,-100), 145));
-    }
-    if ((score/100) % 5 === 0){
-      allEnemies.push(new Enemy(randomLocation(-500,-100), 230));
-    }
-    //
-    score = score + 100;
-    $('#scoreboard').text(score);
-  }
 }
+$('#easy').on("click", function() {
+    level = 'easy';
+    $('#starter').css("display", "none");
+    $('#menu').css("display", "block");
+    $('#score').css("display", "block");
+    $('#lives').css("display", "block");
+    $('#lvlDifficulty').css("display", "block");
+    $('.scoreboard').text(score);
+    $('#livesLeft').text(lives);
+    name = $('#fillName').val();
+    $('.name').text(name);
+    play = true;
+});
+$('#medium').on("click", function() {
+    level = 'medium';
+    $('#starter').css("display", "none");
+    $('#menu').css("display", "block");
+    $('#score').css("display", "block");
+    $('#lives').css("display", "block");
+    $('#lvlDifficulty').css("display", "block");
+    $('.scoreboard').text(score);
+    $('#livesLeft').text(lives);
+    name = $('#fillName').val();
+    $('.name').text(name);
+    play = true;
+});
+$('#hard').on("click", function() {
+    level = 'hard';
+    $('#starter').css("display", "none");
+    $('#menu').css("display", "block");
+    $('#score').css("display", "block");
+    $('#lives').css("display", "block");
+    $('#lvlDifficulty').css("display", "block");
+    $('.scoreboard').text(score);
+    $('#livesLeft').text(lives);
+    name = $('#fillName').val();
+    $('.name').text(name);
+    play = true;
+});
+
+
+function endGame(score) {
+    $('#starter').css("display", "none");
+    $('#menu').css("display", "none");
+    $('#score').css("display", "none");
+    $('#lives').css("display", "none");
+    $('#lvlDifficulty').css("display", "none");
+    $('#endGame').css("display", "block");
+    $('#restart').css("display", "inline-block");
+    $('#replay').css("display", "inline-block")
+    play = false;
+}
+
+$('#replay').on("click", function() {
+    play = true;
+    score = 0;
+    livesLeft = 3;
+    lives = 3
+    player.x = 200;
+    player.y = 400;
+    allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    }
+    $('#starter').css("display", "none");
+    $('#menu').css("display", "block");
+    $('#score').css("display", "block");
+    $('#lives').css("display", "block");
+    $('#lvlDifficulty').css("display", "block");
+    $('#endGame').css("display", "none");
+    $('.scoreboard').text(score);
+    $('#livesLeft').text(lives);
+    name = $('#fillName').val();
+    $('.name').text(name);
+
+    allEnemies = [];
+    allEnemies.push(enemy1, enemy2, enemy3);
+})
