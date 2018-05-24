40d7429fba94b52cb75f967ff20623a1efd2bf11
Dominique Schaefer <Nique0401@yahoo.com>
2018-05-24 02:18:50 -0700

    Created player and edited enemy's to be a level 1 start.  Removed engine.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
-let allEnemies = [];
 let lives;
 let points;
 let level = 'easy';
-let character= 'char-boy.png'
+let character= 'char-boy.png';
+let allEnemies = [];
 
 
 
@@ -17,6 +17,8 @@ var Enemy = function(locX, locY) {
     this.sprite = 'images/enemy-bug.png';
     this.x = locX;
     this.y = locY;
+    this.argument1 = locX;
+    this.argument2 = locY;
 };
 
 // Update the enemy's position, required method for game
@@ -44,9 +46,10 @@ Enemy.prototype.update = function(dt) {
   this.x = this.x+(this.speed*dt);
 
   //resets position if enemy reaches end of screen
-  if (this.x <=500) {
+  if (this.x >=600) {
     this.reset();
   }
+
 
   //collisions with Enemies, reset players position
   if( player.x >= this.x -35 && player.x <=this.x + 35 ){
@@ -55,6 +58,10 @@ Enemy.prototype.update = function(dt) {
               player.y = 400;
           }
       }
+};
+Enemy.prototype.reset = function(){
+    this.x = this.argument1;
+    this.y = this.argument2;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -65,8 +72,47 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+  this.sprite = 'images/'+character;
+  this.x = x;
+  this.y = y;
+};
+Player.prototype.update = function(){
+    //changes, handles and displays the score
+
+};
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function(rcv){
+    if( rcv === 'left' && this.x > 0 )
+        this.x = this.x - 20;
+    else if( rcv === 'right' && this.x < 400)
+        this.x = this.x + 20;
+    else if( rcv === 'up' && this.y > -50)
+        this.y = this.y - 20;
+    else if( rcv === 'down' && this.y < 400)
+        this.y = this.y + 20;
+};
+Player.prototype.reset = function(){
+    this.x = 200;
+    this.y = 400;
+};
+
+var showResult = function(){
+     $('h3').css("display","none").text("");
+};
 
 // Now instantiate your objects.
+let player = new Player(200, 400);
+let enemy1 = new Enemy(-400, 230);
+let enemy2 = new Enemy(-650, 145);
+let enemy3 = new Enemy(-780, 60);
+let enemy4 = new Enemy(-80, 230);
+let enemy5 = new Enemy(-1000, 145);
+let enemy6 = new Enemy(-450, 60);
+
+allEnemies.push(enemy1,enemy2,enemy3,enemy4,enemy5,enemy6);
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
